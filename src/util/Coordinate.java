3fbf20492f2cc281a9029a3f48871175dac05b29
Pooya Shams Kolahi <pooya.shams.k@gmail.com>
2024-01-27 14:44:55 +0330

    Added equals and toString for Coordinates

--- src/util/Coordinate.java
+++ src/util/Coordinate.java
@@ -15,6 +15,20 @@ public class Coordinate
 		this.y = y;
 	}
 
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Coordinate that = (Coordinate) o;
+		return this.x == that.x && this.y == that.y;
+	}
+	@Override
+	public String toString()
+	{
+		return "("+x+", "+y+")";
+	}
+
 	public int getX()
 	{
 		return x;
