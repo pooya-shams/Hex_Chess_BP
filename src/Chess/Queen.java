45e6c945d0b9c87debb9264c89509d9fcadc8c07
Pooya Shams Kolahi <pooya.shams.k@gmail.com>
2024-01-30 19:46:37 +0330

    Added Queen

---
+++ src/Chess/Queen.java
@@ -0,0 +1,27 @@
+package Chess;
+
+import ir.sharif.math.bp02_1.hex_chess.util.PieceName;
+import util.Coordinate;
+import util.HexMat;
+
+import java.util.ArrayList;
+
+public class Queen extends ChessPiece
+{
+	public Queen(boolean is_white, Coordinate pos)
+	{
+		super(is_white, pos);
+		if(is_white)
+			super.piece_name = PieceName.WHITE_QUEEN;
+		else
+			super.piece_name = PieceName.BLACK_QUEEN;
+	}
+
+	@Override
+	public ArrayList<Coordinate> get_valid_moves(HexMat<BoardCell> board)
+	{
+		ArrayList<Coordinate> out = MoveHelper.get_valid_moves(board, this, Bishop.moves);
+		out.addAll(MoveHelper.get_valid_moves(board, this, Rook.moves));
+		return out;
+	}
+}
