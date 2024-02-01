f94c2f435e79a7211395e26ba9854b5782282f3d
Pooya Shams Kolahi <pooya.shams.k@gmail.com>
2024-02-01 17:50:32 +0330

    Added methods inside MoveHelper for checking if king is in check

--- src/Chess/MoveHelper.java
+++ src/Chess/MoveHelper.java
@@ -9,6 +9,7 @@ public class MoveHelper
 {
 	// helps with bishop and rook moves, so we could reuse their move methods without rewriting them in queen
 	// pretty clever, huh?
+	private static final int n = 6;
 	public static ArrayList<Coordinate> get_valid_moves(HexMat<BoardCell> board, ChessPiece in, Coordinate[] moves)
 	{
 		// gets moves as basic vectors of direction for moving
@@ -40,5 +41,42 @@ public class MoveHelper
 				out.add(c);
 		}
 		return out;
+	}
+	private static King get_king(HexMat<BoardCell> board, boolean iw)
+	{
+		King out = null;
+		for(int i = -n+1; i < n; i++)
+		{
+			int l = board.getLen(i);
+			int o = board.getOffset(i);
+			for(int j = -o; j < l-o; j++)
+			{
+				ChessPiece p = board.get(i, j).getContent();
+				if(p instanceof King && p.is_white == iw)
+					out = (King)p;
+			}
+		}
+		if(out == null)
+			throw new IllegalArgumentException("No King of desired color");
+		return out;
+	}
+	public static boolean check_check(HexMat<BoardCell> board, boolean is_white) // gets board and king and returns true if king is in check
+	{
+		King king = get_king(board, is_white);
+		for(int i = -n+1; i < n; i++)
+		{
+			int l = board.getLen(i);
+			int o = board.getOffset(i);
+			for(int j = -o; j < l-o; j++)
+			{
+				ChessPiece p = board.get(i, j).getContent();
+				if(p != null && p.is_white != king.is_white)
+				{
+					if(p.get_valid_moves(board).contains(king.pos))
+						return true;
+				}
+			}
+		}
+		return false;
 	}
 }
