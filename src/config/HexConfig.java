5afdd0c9261a2641cabb44bd4748dd36e4ba81b5
Pooya Shams Kolahi <pooya.shams.k@gmail.com>
2024-02-07 23:29:31 +0330

    Sorted HexConfig file because it was annoying me

--- src/config/HexConfig.java
+++ src/config/HexConfig.java
@@ -7,7 +7,7 @@ public class HexConfig // global configurations
 	public static Color board_panel_color = Color.decode("#52932f");
 	public static Color removed_pieces_panel_color = Color.decode("#31591d");
 	public static Color main_panel_color = Color.decode("#f7f7f7");
-	public static Color[] board_colors = {Color.decode("#e8ab6f"), Color.decode("#ffce9e"), Color.decode("#d18b47")};
 	public static Color board_back_color = Color.decode("#080808");
+	public static Color[] board_colors = {Color.decode("#e8ab6f"), Color.decode("#ffce9e"), Color.decode("#d18b47")};
 	public boolean rotate_board = false;
 }
