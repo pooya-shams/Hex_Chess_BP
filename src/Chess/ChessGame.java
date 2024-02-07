c6615bf50715baef5f0974e2f8fc7e63fed08825
Pooya Shams Kolahi <pooya.shams.k@gmail.com>
2024-02-07 21:18:15 +0330

    Added a ChessGame class and moved all the logic from ChessListener to it

---
+++ src/Chess/ChessGame.java
@@ -0,0 +1,117 @@
+package Chess;
+
+import Chess.ChessBoard;
+import ir.sharif.math.bp02_1.hex_chess.graphics.Application;
+import ir.sharif.math.bp02_1.hex_chess.graphics.listeners.EventListener;
+import util.Coordinate;
+import util.Pair;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class ChessGame
+{
+	private ChessBoard board;
+	private final Application app;
+	private final static File save_file = new File("config/current.hxc");
+	public ChessGame(Application app)
+	{
+		try
+		{
+			load_from(save_file);
+		}
+		catch (FileNotFoundException e)
+		{
+			this.board = new ChessBoard();
+		}
+		this.app = app;
+		if(this.board.draw(app))
+			reset_save_file();
+	}
+
+	private void reset_save_file()
+	{
+		ChessBoard ncb = new ChessBoard();
+		save_board_to_file(save_file, ncb);
+	}
+	private void save_board_to_file(File file, ChessBoard cb)
+	{
+		if(file.getName().contains(".") && (!file.getName().endsWith(".hxc")) )
+		{
+			System.err.println("not a valid filename");
+			return;
+		}
+		else if(!file.getName().contains("."))
+		{
+			file = new File(file.getAbsolutePath()+".hxc");
+		}
+		System.err.println(file.getAbsolutePath());
+		try
+		{
+			PrintStream ps = new PrintStream(file);
+			ps.print(cb.write());
+		}
+		catch (FileNotFoundException e)
+		{
+			System.err.println("no such file directory");
+		}
+	}
+	private void save_to(File file)
+	{
+		save_board_to_file(file, this.board);
+	}
+	private void load_from(File file) throws FileNotFoundException
+	{
+		if(!file.getName().endsWith(".hxc"))
+		{
+			System.err.println("not a valid filename");
+			return;
+		}
+		ChessBoard nb = new ChessBoard();
+		nb.load(file);
+		// assuming there is no error
+		this.board = nb;
+	}
+	public void click(int row, char col)
+	{
+		Coordinate pos = Coordinate.fromGlinski(new Pair<>(col, row));
+		board.click(pos, app);
+		save_to(save_file);
+		if(board.draw(app))
+		{
+			// resetting like an idiot
+			reset_save_file();
+		}
+	}
+	public void load(File file)
+	{
+		try
+		{
+			load_from(file);
+		}
+		catch (FileNotFoundException e)
+		{
+			System.err.println("loading failed");
+		}
+		save_to(save_file);
+		board.draw(app);
+	}
+	public void save(File file)
+	{
+		save_to(file);
+		board.draw(app);
+	}
+	public void newGame()
+	{
+		this.board = new ChessBoard();
+		save_to(save_file);
+		board.draw(app);
+	}
+	public void undo()
+	{
+		// TODO
+	}
+}
--- src/listeners/ChessListener.java
+++ src/listeners/ChessListener.java
@@ -1,6 +1,7 @@
 package listeners;
 
 import Chess.ChessBoard;
+import Chess.ChessGame;
 import ir.sharif.math.bp02_1.hex_chess.graphics.Application;
 import ir.sharif.math.bp02_1.hex_chess.graphics.listeners.EventListener;
 import util.Coordinate;
@@ -14,110 +15,40 @@ import java.util.Scanner;
 
 public class ChessListener implements EventListener // dummy listener honestly
 {
-	private ChessBoard board;
-	private final Application app;
-	private final static File save_file = new File("config/current.hxc");
-	public ChessListener(Application app)
+	private final ChessGame game;
+
+	public ChessListener(Application application)
 	{
-		try
-		{
-			load_from(save_file);
-		}
-		catch (FileNotFoundException e)
-		{
-			this.board = new ChessBoard();
-		}
-		this.app = app;
-		if(this.board.draw(app))
-			reset_save_file();
+		this.game = new ChessGame(application);
 	}
 
-	private void reset_save_file()
-	{
-		ChessBoard ncb = new ChessBoard();
-		save_board_to_file(save_file, ncb);
-	}
-	private void save_board_to_file(File file, ChessBoard cb)
-	{
-		if(file.getName().contains(".") && (!file.getName().endsWith(".hxc")) )
-		{
-			System.err.println("not a valid filename");
-			return;
-		}
-		else if(!file.getName().contains("."))
-		{
-			file = new File(file.getAbsolutePath()+".hxc");
-		}
-		System.err.println(file.getAbsolutePath());
-		try
-		{
-			PrintStream ps = new PrintStream(file);
-			ps.print(cb.write());
-		}
-		catch (FileNotFoundException e)
-		{
-			System.err.println("no such file directory");
-		}
-	}
-	private void save_to(File file)
-	{
-		save_board_to_file(file, this.board);
-	}
-	private void load_from(File file) throws FileNotFoundException
-	{
-		if(!file.getName().endsWith(".hxc"))
-		{
-			System.err.println("not a valid filename");
-			return;
-		}
-		ChessBoard nb = new ChessBoard();
-		nb.load(file);
-		// assuming there is no error
-		this.board = nb;
-	}
 	@Override
 	public void onClick(int row, char col)
 	{
-		Coordinate pos = Coordinate.fromGlinski(new Pair<>(col, row));
-		board.click(pos, app);
-		save_to(save_file);
-		if(board.draw(app))
-		{
-			// resetting like an idiot
-			reset_save_file();
-		}
+		this.game.click(row, col);
 	}
+
 	@Override
 	public void onLoad(File file)
 	{
-		try
-		{
-			load_from(file);
-		}
-		catch (FileNotFoundException e)
-		{
-			System.err.println("loading failed");
-		}
-		save_to(save_file);
-		board.draw(app);
+		this.game.load(file);
 	}
+
 	@Override
 	public void onSave(File file)
 	{
-		save_to(file);
-		board.draw(app);
+		this.game.save(file);
 	}
+
 	@Override
 	public void onNewGame()
 	{
-		this.board = new ChessBoard();
-		save_to(save_file);
-		board.draw(app);
+		this.game.newGame();
 	}
 
 	@Override
 	public void undo()
 	{
-		// TODO
+		this.game.undo();
 	}
 }
